42b8b89d03ab5ed733593521796065ce4c4662d0
Michael W Lee <mikewclee@gmail.com>
2020-06-16 18:33:41 -0400

    readme, index, css and js.

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,51 +1,61 @@
-setInterval(function () {
-    var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-    // console.log(todayDate);
-    $('#clock').html(todayDate);
+$(document).ready(function () {
 
-}, 1000);
+    setInterval(function () {
+        var todayDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+        // console.log(todayDate);
+        $('#clock').html(todayDate);
 
-var apiKey = '&APPID=8f3ed085f2c3b566fb3eb9e5a44a0736';
+    }, 1000);
 
-$('#search-button').on('click', function (event) {
-    event.preventDefault();
-    var cityInput = $("#search-value").val();
-    var cityList = $("<button>").attr({ id: "city" }).html(cityInput);
-    $('.list-group').prepend(cityList);
+    var apiKey = '&APPID=8f3ed085f2c3b566fb3eb9e5a44a0736';
 
+    $('#search-button').on('click', function (event) {
+        event.preventDefault();
+        //store user input
+        var cityInput = $("#search-value").val();
+        //array to store enter Cities from user
+        var allCities = [];
+        allCities = JSON.parse(localStorage.getItem("allCities")) || [];
+        allCities.push(cityInput); // pushes new cities entered to array 
+        localStorage.setItem("allCities", JSON.stringify(allCities)); //saves city input to local storage 
+        // console.log('local storage: ' + allCities)
+        //get current weather function
+        getWeather(cityInput);
+    })
 
-    console.log("User type: " + cityInput);
-    // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+ cityInput + apiKey;
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput +"&units=imperial" + apiKey;
-    console.log("API: " + queryURL);
+    function getWeather(cityInput) {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (response) {
-        var todayDate = moment().format('LL');
-        var weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-        var lat = response.coord.lat;
-        var lon = response.coord.lon;
+        console.log("User type: " + cityInput);
+        // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+ cityInput + apiKey;
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial" + apiKey;
+        console.log("API: " + queryURL);
 
-        //Generate currentWeather info
-        $("#currentWeather").html(`
-        <h2>${response.name} (${todayDate})<img src=${weatherIcon} height="70px"></h2
-        <p>Temperature: ${response.main.temp} &#176;C</p>
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            // var todayDate = moment().format('LL');
+            var todayDate = new Date(response.dt * 1000).toLocaleDateString("en-US");
+            var weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            //Generate currentWeather info
+            $("#currentWeather").html(`
+        <h2>${response.name}, ${response.sys.country} (${todayDate})<img src=${weatherIcon} height="70px"></h2
+        <p>Temperature: ${response.main.temp}&#176;F</p>
         <p>Humidity: ${response.main.humidity}%</p>
         <p>Wind Speed: ${response.wind.speed} m/s</p>
         `,
-        //uvIndex function(lat,lon)
-            uvIndex(lat, lon))
-        // createHistoryBtn(response.name);
-    })
+                //uvIndex function(lat,lon)
+                uvIndexForcast(lat, lon))
+            //saved cities function
+            savedCities();
+        })
+    }
 
-
-    
-
-    // The current UV index is collected at the same time as the current weather
-    // by making use of the searched city's returned coordinates
-    function uvIndex(lat, lon) {
+    // UV index and 5 day forcast from lat and lon info
+    function uvIndexForcast(lat, lon) {
         var uvqueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial" + apiKey;
         console.log('uvqquery function:' + uvqueryURL);
 
@@ -64,28 +74,28 @@ $('#search-button').on('click', function (event) {
             } else if (currUVIndex >= 3) {
                 uvSeverity = "yellow";
             }
-            $("#currentWeather").append(`<p>UV Index: <span class="text-black p-1" style="background-color: ${uvSeverity};">${currUVIndex}</span></p>`);
-        
+            $("#currentWeather").append(`<p>UV Index: <span class="text-black uvStyle" style="background-color: ${uvSeverity};"> ${currUVIndex} </span></p>
+            <h2>Current Weather</h2> 
+            <h3>5-Day Forcast</h3>`);
+
             //empty 5day forcast
             $('#weatherForecast').empty();
-
-            for(var i=1; i< response.daily.length -2 ; i++){
+            //Loop thru for 5 day forcast
+            for (var i = 1; i < response.daily.length - 2; i++) {
                 var forcastDate = response.daily[i].dt;
-                // console.log('forcast Date Epoch:' + forcastDate);
-                //convert unix time to EST
-                var newDate= new Date(forcastDate * 1000).toLocaleDateString("en-US");
-                console.log('forcast Date:' + newDate);
+                //convert Epoch time to EST
+                var newDate = new Date(forcastDate * 1000).toLocaleDateString("en-US");
+                // console.log('forcast Date:' + newDate);
                 var iconPic = response.daily[i].weather[0].icon;
-                console.log('icon weather; '+iconPic);
                 var weatherIcon = `https://openweathermap.org/img/wn/${iconPic}.png`;
-
+                //append a day forcast
                 $('#weatherForecast').append(`
                 <div class="col-sm">
                     <div class="card  text-white bg-primary">
                         <div class="card-body text-center">
                             <h4>${newDate} </h4>
                             <img src=${weatherIcon} alt="Icon">
-                            <p>Temp: ${response.daily[i].temp.day} &#176;C</p>
+                            <p>Temp: ${response.daily[i].temp.day} &#176;F</p>
                             <p>Humidity: ${response.daily[i].humidity}%</p>
                         </div>
                     </div>
@@ -96,4 +106,25 @@ $('#search-button').on('click', function (event) {
         })
     }
 
-})
+    function savedCities() {
+        $("#cityButtons").empty(); // empties out previous array 
+        var arrayFromStorage = JSON.parse(localStorage.getItem("allCities")) || []; // Makes all cities searched a string
+        var arrayLength = arrayFromStorage.length; // limits length of array
+
+        for (var i = 0; i < arrayLength; i++) {
+            var cityNameFromArray = arrayFromStorage[i]; //
+
+            $("#cityButtons").prepend(`<div class='list-group'> <button class='list-group-item'> ${cityNameFromArray}</button>`)
+        }
+    }
+
+    savedCities(); // calls function to append cities upon page load 
+
+    // show cities on click 
+    $("#cityButtons").on("click", ".list-group-item", function (event) {
+        event.preventDefault();
+        var cityInput = ($(this).text());
+        getWeather(cityInput);
+    })
+
+});
--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+This app is a weather dashboard that allows the user to input a city and display the current weather and 5 day forecast. 
+The user's search history is saved as a button and allows them to quickly check the weather of that city by clicking it.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+We use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. 
 
-## User Story
+## Design
 
 ```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
 ```
 
 ## Acceptance Criteria
@@ -32,12 +30,4 @@ THEN I am presented with the last searched city forecast
 
 The following image demonstrates the application functionality:
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+![weather dashboard demo](./Assets/images/weather.JPG)
